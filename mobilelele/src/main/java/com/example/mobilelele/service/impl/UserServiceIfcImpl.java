208883691b75a06b42d03742f7ed205f79b0dca4
rahvaliev <t.rahvaliev@inkofoods.com>
2023-01-09 18:39:29 +0200

    logger

--- mobilelele/src/main/java/com/example/mobilelele/service/impl/UserServiceIfcImpl.java
+++ mobilelele/src/main/java/com/example/mobilelele/service/impl/UserServiceIfcImpl.java
@@ -1,15 +1,32 @@
 package com.example.mobilelele.service.impl;
 
+import com.example.mobilelele.model.entity.UserEntity;
 import com.example.mobilelele.model.service.UserLoginServiceModel;
+import com.example.mobilelele.repository.UserRepository;
 import com.example.mobilelele.service.UserServiceIfc;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceIfcImpl implements UserServiceIfc {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public UserServiceIfcImpl(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public boolean login(UserLoginServiceModel userLoginServiceModel) {
-        //todo login
-        throw new UnsupportedOperationException("Not yet implemented");
+        Optional<UserEntity> userEntityOpt = userRepository.findByUsername(userLoginServiceModel.getUsername());
+        if(userEntityOpt.isEmpty()){
+            return false;
+        }else {
+           return passwordEncoder.matches(userLoginServiceModel.getPassword(), userEntityOpt.get().getPassword());
+        }
 
     }
 }
--- mobilelele/src/main/java/com/example/mobilelele/web/UserLoginController.java
+++ mobilelele/src/main/java/com/example/mobilelele/web/UserLoginController.java
@@ -3,6 +3,8 @@ package com.example.mobilelele.web;
 import com.example.mobilelele.model.binding.UserLoginBindingModel;
 import com.example.mobilelele.model.service.UserLoginServiceModel;
 import com.example.mobilelele.service.UserServiceIfc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserLoginController {
     private final UserServiceIfc userServiceIfc;
+    private static Logger LOGGER = LoggerFactory.getLogger(UserLoginController.class);
 
     public UserLoginController(UserServiceIfc userServiceIfc) {
         this.userServiceIfc = userServiceIfc;
@@ -23,11 +26,16 @@ public class UserLoginController {
     @PostMapping("/users/login")
     public String login(UserLoginBindingModel userLoginBindingModel){
 
+
         //delegate the logic to the service layer
 
        boolean loginSuccessfully= userServiceIfc.login(new UserLoginServiceModel().setUsername(userLoginBindingModel.getUsername())
                 .setPassword(userLoginBindingModel.getPassword()));
-        // TODO login
-        return "redirect:/index";
+
+        LOGGER.info("User tried to login.User with name {} try to login , success {}",
+                userLoginBindingModel.getUsername(),
+               loginSuccessfully);
+
+        return "redirect:/users/login";
     }
 }
